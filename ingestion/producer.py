c2315195e6f74feefd734dc14dcde009cc352530
ntejha <ntejha04@gmail.com>
2025-07-03 19:11:18 +0530

    change of plans

--- ingestion/producer.py
+++
@@ -1,9 +0,0 @@
-# ingestion/producer.py
-from ingestion.fetch_news import get_news
-from ingestion.fetch_stocks import get_prices
-
-def fetch_data():
-    news_data = get_news()
-    stock_data = get_prices()
-    return news_data, stock_data
-
--- processing/sentiment.py
+++
@@ -1,7 +0,0 @@
-from textblob import TextBlob
-
-def analyze_sentiment(news_list):
-    for item in news_list:
-        blob = TextBlob(item["headline"])
-        item["sentiment"] = blob.sentiment.polarity
-    return news_list
